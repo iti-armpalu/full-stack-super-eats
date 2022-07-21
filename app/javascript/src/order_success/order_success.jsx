935fe682c7a6429c49e9058b8745fdc312f4d98c
Iti <iti.armpalu@gmail.com>
2022-07-21 08:50:32 +0400

    Order success responsive CSS fix

--- app/javascript/src/order_success/order_success.jsx
+++ app/javascript/src/order_success/order_success.jsx
@@ -56,27 +56,27 @@ class OrderSuccess extends React.Component {
   return (
     <Layout>
       <div className="mt-80 pb-40">
-          <div className="pl-40 pr-40">
-            <div className="mb-70">
-              <h1 className="text-center mb-10">
+          <div className="container">
+            <div className="md-mb-40 mb-70">
+              <h1 className="text-center mb-10 md-mb-20">
                 Your order from <span className="order-restaurant-name text-decoration-underline">{restaurant.name}</span> is confirmed!
               </h1>
-              <div className="d-flex justify-content-center">
-                <p className="order-number">
+              <div className="d-block d-md-flex justify-content-center">
+                <p className="mb-5 mb-md-0 text-center">
                   Order ID: <span>#{order.id}</span>
                 </p>
-                <span className="px-2"> · </span>
-                <p className="order-date">
+                <span className="d-none d-md-block px-2"> · </span>
+                <p className="mb-5 mb-md-0 text-center">
                   Order submitted: <span>{FormatDate(order.created_at)}</span>
                 </p>
-                <span className="px-2"> · </span>
-                <p className="order-date">
+                <span className="d-none d-md-block px-2"> · </span>
+                <p className="mb-5 mb-md-0 text-center">
                   Order total: <span>$ {this.getTotal(order.subtotal, restaurant.delivery_fee)}</span>
                 </p>
               </div>
             </div>
-            <div className="row d-flex justify-content-between gx-0 pt-40 pb-40 pl-60 pr-60">
-              <div className="col-5 pl-50 pr-50">
+            <div className="row d-flex justify-content-between gx-0 pt-40 pb-40 pl-60 pr-60 order-details-wrap">
+              <div className="col-12 col-lg-6 col-xl-5 pl-50 pr-50  order-2 order-lg-1 order-info-wrap">
                 <div className="d-flex flex-column">
                   <div className="d-flex mb-40">
                     <div className="mr-20 my-auto">
@@ -122,11 +122,11 @@ class OrderSuccess extends React.Component {
                       <p className="order-delivery-address mb-1">
                         Delivery partner
                       </p>
-                      <div className="d-flex">
+                      <div className="d-block d-sm-flex d-lg-block d-xl-flex">
                       <h6 className="mb-5">
                         {delivery_user.first_name} {delivery_user.last_name}
                       </h6>
-                      <span className="mx-2"> · </span>
+                      <span className="d-none d-sm-block d-lg-none d-xl-block mx-2"> · </span>
                       <h6>
                         <FontAwesomeIcon  icon={ faPhone } className="mr-10 icon-phone"  /> 
                         {delivery_user.phone_number}
@@ -134,8 +134,8 @@ class OrderSuccess extends React.Component {
                       </div>
                     </div>
                   </div>
-                  <div className="border-top pt-40 pb-10">
-                    <p className="pr-50">
+                  <div className="border-top pt-40 pb-10 text-center text-lg-start">
+                    <p className="md-pr-50">
                       You can always view your order details in your order history:
                     </p>
                     <a className="btn btn-order-history text-uppercase mt-20 mb-20" 
@@ -146,8 +146,8 @@ class OrderSuccess extends React.Component {
                   </div>
                 </div>
               </div>
-              <div className="col-7 d-flex">
-              <div className="map-area-one me-lg-4 md-mt-50">
+              <div className="col-12 col-lg-6 col-xl-7 d-flex order-1 order-lg-2">
+              <div className="map-area me-lg-4 md-mb-50">
 									<div className="mapouter">
 										<div className="gmap_canvas">
                       <iframe className="gmap_iframe" src={restaurant.address_url}></iframe>
