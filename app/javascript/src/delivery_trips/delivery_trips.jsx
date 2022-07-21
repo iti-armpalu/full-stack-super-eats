7ed93baa99814e29e99265de65b62996eb79260c
Iti <iti.armpalu@gmail.com>
2022-07-21 11:45:33 +0400

    Delivery trips responsive CSS fix

--- app/javascript/src/delivery_trips/delivery_trips.jsx
+++ app/javascript/src/delivery_trips/delivery_trips.jsx
@@ -37,7 +37,6 @@ class DeliveryTrips extends React.Component {
       })
   }
 
-
   render () {
     const { deliveryTrips, loading } = this.state;
 
@@ -45,94 +44,120 @@ class DeliveryTrips extends React.Component {
       return <p>Loading...</p>;
     };
 
-    // const {
-    //   order
-    // } = deliveryTrips
-
     return (
       <Layout>
         <div className="mt-80 pb-40">
-          <div className="pl-40 pr-40">
+          <div className="container">
             <h3 className="mb-50">My trips</h3>
 
             {(deliveryTrips.length != 0)
 
               ?
               <div>
-                <div className="reservations-header-row p-4 mb-20 border-bottom">
-                  <div className="row no-gutters d-flex justify-content-between align-items-center text-center">
+                <div className="d-none d-lg-block p-4 mb-20 border-bottom">
+                  <div className="row gx-0 d-flex justify-content-between align-items-center text-center">
                     <div className="col-1">
-                      <p className="mb-2">Delivery ID</p>
+                      <p className="mb-10">Delivery ID</p>
                     </div>
                     <div className="col-2">
-                      <p className="mb-2">Delivery date</p>
+                      <p className="mb-10">Delivery date</p>
                     </div>
                     <div className="col-3">
-                    
-                      <p className="mb-2">Pick-up details</p>
+                      <p className="mb-10">Pick-up details</p>
                     </div>
                     <div className="col-3">
-                      <p className="mb-2">Drop-off details</p>
+                      <p className="mb-10">Drop-off details</p>
                     </div>
                     <div className="col-2">
-                      <p className="mb-2">Delivery earning</p>
+                      <p className="mb-10">Delivery earning</p>
                     </div>
                   </div>
                 </div>
-                
-                {deliveryTrips.map(delivery => {
-                  return (
-                    <div key={delivery.id} id={delivery.id} className="trips-wrap p-4 mb-20 rounded">
-                      <div className="row no-gutters d-flex justify-content-between align-items-center text-center">
-                        <div className="col-1">
-                          <p className="mb-2"># {delivery.id}</p>
-                        </div>
-                        <div className="col-2">
-                          <p className="mb-2">{FormatDate(delivery.created_at)}</p>
-                        </div>
-                        <div className="col-3">
-                          <div className="d-flex flex-column">
-                            <p className="mb-10">
-                              {delivery.restaurant.name}
-                            </p>
-                            <p>
-                              {delivery.restaurant.address},
-                            </p>
-                            <p className="mb-10">
-                              {delivery.restaurant.city}, {delivery.restaurant.country}
-                            </p>
-                            <button type="button" className="btn btn-trip-contact btn-sm mx-5">Message restaurant</button>
+
+                <div className="row">
+
+                  {deliveryTrips.map(delivery => {
+                    return (
+                      <div 
+                        key={delivery.id} 
+                        id={delivery.id} 
+                        className="col-12 col-sm-6 col-lg-12">
+                          <div className="trips-wrap p-4 mb-20 rounded ">
+                          <div className="row gx-0 d-flex justify-content-between align-items-center text-center">
+                            <div className="col-12 col-lg-1">
+                              <p className="mb-10">
+                                # {delivery.id}
+                              </p>
+                            </div>
+                            <div className="col-12 col-lg-2">
+                              <p className="mb-10">
+                                {FormatDate(delivery.created_at)}
+                              </p>
+                            </div>
+                            <div className="col-12 col-lg-3">
+                              <div className="d-flex flex-column md-mb-40 md-mt-40 mb-0 mt-0">
+                                <p className="d-block d-lg-none mb-10">
+                                  <b>Pick-up details</b>
+                                </p>
+                                <p className="mb-10">
+                                  {delivery.restaurant.name}
+                                </p>
+                                <p>
+                                  {delivery.restaurant.address},
+                                </p>
+                                <p className="mb-10">
+                                  {delivery.restaurant.city}, {delivery.restaurant.country}
+                                </p>
+                                <button 
+                                  type="button" 
+                                  className="btn btn-trip-contact btn-sm mx-auto w-75">
+                                    Message restaurant
+                                </button>
+                              </div>
+                            </div>
+                            <div className="col-12 col-lg-3">
+                              <div className="d-flex flex-column md-mb-40 mb-0">
+                                <p className="d-block d-lg-none mb-10">
+                                  <b>Drop-off details</b>
+                                </p>
+                                <p className="mb-10">
+                                  {delivery.user.first_name} {delivery.user.last_name}
+                                </p>
+                                <p>
+                                  {delivery.user.address},
+                                </p>
+                                <p className="mb-10">
+                                  {delivery.user.city}, {delivery.user.country}
+                                </p>
+                                <button 
+                                  type="button" 
+                                  className="btn btn-trip-contact btn-sm mx-auto w-75">
+                                    Message {delivery.user.first_name}
+                                </button>
+                              </div>
+                            </div>
+                            <div className="col-12 col-lg-2">
+                              <p className="d-block d-lg-none mb-10">
+                                <b>Delivery earning</b>
+                              </p>
+                              <p className="trip-earning">
+                                <b>$ {delivery.restaurant.delivery_fee}.00</b></p>
+                            </div>
                           </div>
                         </div>
-                        <div className="col-3">
-                          <div className="d-flex flex-column">
-                            <p className="mb-10">
-                              {delivery.user.first_name} {delivery.user.last_name}
-                            </p>
-                            <p>
-                              {delivery.user.address},
-                            </p>
-                            <p className="mb-10">
-                              {delivery.user.city}, {delivery.user.country}
-                            </p>
-                            <button type="button" className="btn btn-trip-contact btn-sm mx-5">Message {delivery.user.first_name}</button>
-                          </div>
-                        </div>
-                        <div className="col-2">
-                          <p className="trip-earning"><b>$ {delivery.restaurant.delivery_fee}.00</b></p>
-                        </div>
-                      </div>
                     </div>
-                  )
-                })}
+                    )
+                  })}
+                </div>
               </div>
 
               :
               <div className="border border-secondary rounded text-center">
-                <p className="py-4">You don't have any upcoming trips at the moment.</p>
+                <p className="py-4">
+                  You don't have any upcoming trips at the moment.
+                </p>
               </div>
             }
-        
           </div>
         </div>
       </Layout>
--- app/javascript/src/orders/orders.jsx
+++ app/javascript/src/orders/orders.jsx
@@ -57,52 +57,55 @@ class Orders extends React.Component {
             <div className="row">
               {userOrders.map(order => {
                 return (
-                  <div key={order.id} id={order.id} className="col-4 d-flex flex-column mb-20">
-                    <div className="bg-light rounded px-3 py-3">
-                      <div className="aspect-ratio-rectangle rounded mb-10" style={{ backgroundImage: `url(${order.restaurant.image_url})` }} />
-                      <h5 className="mb-5">
-                        {order.restaurant.name}
-                      </h5>
-                      <p className="order-details mb-10">
-                        {FormatDate(order.created_at)}
-                      </p>
-                      <p>
-                        Liked the restaurant?
-                        <a className="btn btn-view-menu text-decoration-underline py-0 mx-2" role="button" href={`/restaurant/${order.restaurant.id}`}>
-                          Order again
-                        </a>
-                      </p>
-                      <div className="divider my-3"></div>
-                      <div>
-                        <h6 className="mb-10">
-                          Your order details:
-                        </h6>
-                        <div className="d-flex justify-content-between px-3 order-details">
-                          <p className="mb-5">
-                            Subtotal
-                          </p>
-                          <p className="mb-5">
-                            $ {Number(order.subtotal).toFixed(2)}
-                          </p>
-                        </div>
-                        <div className="d-flex justify-content-between px-3 order-details">
-                          <p className="mb-5">
-                            Delivery fee
-                          </p>
-                          <p className="mb-5">
-                            $ {Number(order.restaurant.delivery_fee).toFixed(2)}
-                          </p>
-                        </div>
-                        <div className="order-total d-flex justify-content-between px-3">
-                          <p>
-                            <b>Total</b>
-                          </p>
-                          <p>
-                            <b>$ {this.getTotal(order.subtotal, order.restaurant.delivery_fee)}</b>
-                          </p>
+                  <div 
+                    key={order.id} 
+                    id={order.id} 
+                    className="col-12 col-md-6 col-lg-4 d-flex flex-column mb-20">
+                      <div className="bg-light rounded px-3 py-3">
+                        <div className="aspect-ratio-rectangle rounded mb-10" style={{ backgroundImage: `url(${order.restaurant.image_url})` }} />
+                        <h5 className="mb-5">
+                          {order.restaurant.name}
+                        </h5>
+                        <p className="order-details mb-10">
+                          {FormatDate(order.created_at)}
+                        </p>
+                        <p>
+                          Liked the restaurant?
+                          <a className="btn btn-view-menu text-decoration-underline py-0 mx-2" role="button" href={`/restaurant/${order.restaurant.id}`}>
+                            Order again
+                          </a>
+                        </p>
+                        <div className="divider my-3"></div>
+                        <div>
+                          <h6 className="mb-10">
+                            Your order details:
+                          </h6>
+                          <div className="d-flex justify-content-between px-3 order-details">
+                            <p className="mb-5">
+                              Subtotal
+                            </p>
+                            <p className="mb-5">
+                              $ {Number(order.subtotal).toFixed(2)}
+                            </p>
+                          </div>
+                          <div className="d-flex justify-content-between px-3 order-details">
+                            <p className="mb-5">
+                              Delivery fee
+                            </p>
+                            <p className="mb-5">
+                              $ {Number(order.restaurant.delivery_fee).toFixed(2)}
+                            </p>
+                          </div>
+                          <div className="order-total d-flex justify-content-between px-3">
+                            <p>
+                              <b>Total</b>
+                            </p>
+                            <p>
+                              <b>$ {this.getTotal(order.subtotal, order.restaurant.delivery_fee)}</b>
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
                   </div>
                 )
               })}
